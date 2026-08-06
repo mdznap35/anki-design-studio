0a119c249dd03fc03d41459ac92c5f6a54ae0f41
Anki Studio <anki@studio.com>
2026-08-06 17:43:16 +0300

    دعم CORS للتصدير من أي مكان + مسار API ذكي (Render/محلي/Pages)

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ from flask import Flask, request, jsonify, send_file, send_from_directory
 
 app = Flask(__name__, static_folder='public')  # nosec S4502
 
+@app.after_request
+def add_cors_headers(response):  # nosec SXXX
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+@app.route('/api/generate', methods=['OPTIONS'])
+def generate_preflight():
+    return '', 204
+
 VOCAB_DATA = None
 REPEATED_DATA = None
 try:
